8d5ddda655c126f7e0048afec88b1726b9fb6fea
Pesilian <linasignell@live.se>
2025-02-27 11:38:45 +0100

    styled homepage

--- src/app/components/formLink.tsx
+++ src/app/components/formLink.tsx
@@ -1,9 +1,15 @@
 import Link from 'next/link'
+import { MdLaunch } from 'react-icons/md'
 
 export default function FormLink() {
   return (
-    <Link href="/transcriptionform" className="bg-blue">
-      Påbörja transkription
+    <Link
+      href="/transcriptionform"
+      className="text-md font-Roboto hover:text-stone-400 flex mt-4"
+    >
+      <span className="mr-2"> Påbörja ny transkription</span>
+
+      <MdLaunch className="self-center" />
     </Link>
   )
 }
