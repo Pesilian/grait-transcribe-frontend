81deec774c688158d839422a133a8d5da0f4a636
Pesilian <linasignell@live.se>
2025-02-28 09:55:39 +0100

    added argin

--- src/app/transcribed/raw/page.tsx
+++ src/app/transcribed/raw/page.tsx
@@ -5,7 +5,7 @@ import RawTranscription from '../features/transcriptionTextRaw'
 
 export default function Diarized() {
   return (
-    <article className="h-full w-full grid grid-cols-7 gap-4 content-start">
+    <article className="h-full w-full grid grid-cols-7 gap-4 content-start mt-8">
       <AudioIcon />
       <TranscriptionHeader />
       <PagesButton />
