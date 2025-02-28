7ada667d75533a79b40cf15c4b0222fa84590ccf
Pesilian <linasignell@live.se>
2025-02-28 15:00:54 +0100

    changed path

--- src/app/components/formLink.tsx
+++ src/app/components/formLink.tsx
@@ -4,7 +4,7 @@ import { MdLaunch } from 'react-icons/md'
 export default function FormLink() {
   return (
     <Link
-      href="/transcriptionform"
+      href="/newtranscription"
       className="text-md font-Roboto hover:text-stone-400 flex mt-4"
     >
       <span className="mr-2"> Påbörja ny transkription</span>
